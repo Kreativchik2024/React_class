9eebcceaef1463553c12d88533d3feb4fe7d4e94
Kreativchik69 <kreativchik69@gmail.com>
2026-01-04 23:21:56 +0300

    step3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react"
 function App() {
+  const [count, setCount] = useState(0);
     return (
     <>
-      Hallo World
+      <h1>Счетчик кликов</h1>
+      <button onClick={() => setCount(count+1)}>
+      Клик:   {count}
+      </button>
     </>
   )
 }
